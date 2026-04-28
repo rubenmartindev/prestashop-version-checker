15b009d00cdd2cdab58f2c3370087daffd6f117b
hasdpk <hasdpk@gmail.com>
2026-04-28 16:04:25 +0100

    test: add PHPUnit testing setup

---
+++ tests/Unit/HealtyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RubenMartinDev\PrestaShopVersionChecker\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HealtyTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
