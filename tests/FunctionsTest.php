4b87ebb119f62d61aee15450494350ad5656e748
Rubén Martín <hasdpk@gmail.com>
2026-05-17 05:56:35 +0100

    feat: catch exceptions in is_ps_version() function and return false

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -14,20 +14,46 @@ class FunctionsTest extends TestCase
         $this->assertTrue(function_exists('is_ps_version'));
     }
 
-    public function testIsPsVersionReturnsTrueWhenVersionMatches()
+    public function testIsPsVersionFunctionReturnFalseWhenPrestaShopNotInitialized()
+    {
+        $this->assertFalse(\is_ps_version('>1.7'));
+    }
+
+    public function testIsPsVersionFunctionReturnFalseWhenCompareIsNotString()
     {
         $this->setPrestaShopVersion();
 
-        $this->assertTrue(is_ps_version('<1.7'));
-        $this->assertTrue(is_ps_version('>=1.6'));
+        $this->assertFalse(\is_ps_version(1.1));
+    }
+
+    public function testIsPsVersionFunctionReturnFalseWhenInvalidOperator()
+    {
+        $this->setPrestaShopVersion();
+
+        $this->assertFalse(\is_ps_version('a1.7'));
+    }
+
+    public function testIsPsVersionFunctionReturnFalseWhenInvalidVersionNumber()
+    {
+        $this->setPrestaShopVersion();
+
+        $this->assertFalse(\is_ps_version('>a'));
     }
 
     public function testIsPsVersionReturnsFalseWhenVersionDoesNotMatch()
     {
         $this->setPrestaShopVersion();
 
-        $this->assertFalse(is_ps_version('>=1.7'));
-        $this->assertFalse(is_ps_version('<1.6'));
+        $this->assertFalse(\is_ps_version('>=1.7'));
+        $this->assertFalse(\is_ps_version('<1.6'));
+    }
+
+    public function testIsPsVersionReturnsTrueWhenVersionMatches()
+    {
+        $this->setPrestaShopVersion();
+
+        $this->assertTrue(\is_ps_version('<1.7'));
+        $this->assertTrue(\is_ps_version('>=1.6'));
     }
 
     private function setPrestaShopVersion()
